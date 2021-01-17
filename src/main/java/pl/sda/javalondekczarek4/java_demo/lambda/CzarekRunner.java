71de9b71341ddc972bd222c1af4d3c99afa87ac8
Czarek <cfryszkowski@gmail.com>
2021-01-17 12:42:06 +0000

    [add] - WorkLifeBalance and WorkLifeBalanceImplementation

--- src/main/java/pl/sda/javalondekczarek4/java_demo/lambda/CzarekRunner.java
+++ src/main/java/pl/sda/javalondekczarek4/java_demo/lambda/CzarekRunner.java
@@ -2,6 +2,11 @@ package pl.sda.javalondekczarek4.java_demo.lambda;
 
 public class CzarekRunner implements Runner{
     @Override
+    public void go(int speed) {
+        System.out.println("Czarek speed is"+speed);
+    }
+
+    @Override
     public void go() {
         System.out.println("Run Czarek, run");
     }
