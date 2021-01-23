2309090249079093bd118b6c9d24e3093aa83fa4
Czarek <cfryszkowski@gmail.com>
2021-01-23 10:24:45 +0000

    [change] nickDealer

--- read-me.md
+++ read-me.md
@@ -2,3 +2,9 @@ Git - good practices
 
 * commit powinien miec dobry opis
 * commit powinen miec jedna zmiane
+*zmiana opisu tylko dla ostatiego commita (amend)
+
+Git - przydatne komendy
+git status
+git add .
+git commit -m "[tag] - message"
--- src/main/java/pl/sda/javalondekczarek4/java_demo/exception/ExceptionExample.java
+++ src/main/java/pl/sda/javalondekczarek4/java_demo/exception/ExceptionExample.java
@@ -22,7 +22,6 @@ public class ExceptionExample {
         }
         System.out.println("My nick is: " + myNick);
         System.out.println("Now with runtime exceptions...");
-        myNick = exceptionGenerator.nickWithRuntimeException();
         try {
             myNick= exceptionGenerator.nickWithRuntimeException();
         }catch (NoNickRuntimeException e){
@@ -32,6 +31,21 @@ public class ExceptionExample {
         System.out.println("My nick now is: " + myNick);
 //        String nullString = null;
 //        nullString.concat(" ma kota");
+        System.out.println("With dealer:):):)");
+        NickDealer nickDealer = new NickDealer(new ExceptionGenerator());
+        String myNickDealer;
+        try {
+            myNickDealer =  nickDealer.nickFromDealer();
+        } catch (NoNickException e) {
+            myNickDealer = "default nick name";
+        }
+        System.out.println("Nick from myDealer: " + myNickDealer);
+        try {
+            myNickDealer = nickDealer.nick();
+        } catch (NoNickRuntimeException exc) {
+            myNickDealer = "unexpected value....";
+        }
+        System.out.println("Nick from dealer: " + myNickDealer);
         System.out.println("here???");
     }
 }
