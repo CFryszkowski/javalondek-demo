e5c39f04a63c2d783650e01bcd34ea6aa18a64cd
Czarek <cfryszkowski@gmail.com>
2021-01-24 11:11:26 +0000

    [add] - new class for storin secrets

---
+++ src/main/java/pl/sda/javalondekczarek4/java_demo/optional/MySecret.java
@@ -0,0 +1,44 @@
+package pl.sda.javalondekczarek4.java_demo.optional;
+
+import static java.util.Objects.nonNull;
+
+public class MySecret {
+    private String secret;
+
+    @Override
+    public String toString() {
+        return "MySecret{" +
+                "secret='" + secret + '\'' +
+                '}';
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    public MySecret(String secret) {
+        this.secret = secret;
+    }
+
+    public static void main(String[] args) {
+        MySecret realOne = new MySecret("bla bla bla bla bla bla bla bla bla bla bla bla bla");
+        MySecret withNullContent = new MySecret(null);
+        printSecretLength(realOne);
+        printSecretLength(withNullContent);
+    }
+
+    public static void printSecretLength(MySecret mySecret){
+        int secretLen = mySecret.getSecret()!=null? mySecret.getSecret().length() : 0;
+         secretLen = nonNull( mySecret.getSecret())? mySecret.getSecret().length() : 0;
+//        if (mySecret.getSecret() != null){
+//            secretLen = mySecret.getSecret().length();
+//        }else {
+//            secretLen = 0;
+//        }
+        System.out.println("My secret has " + secretLen + " characters");
+    }
+}
